5cf38ee19be8861eeab39291d43b7925ac795465
mscafuro13 <mscafuro13@gmail.com>
2018-06-09 23:19:57 -0400

    Adding first 2 plots

---
+++ plot1.R
@@ -0,0 +1,23 @@
+#Read in file
+powerConsumption <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+
+#Convert character string to Date
+powerConsumption$Date <- as.Date(powerConsumption$Date, "%d/%m/%Y")
+
+#Subset to dates of interest
+powerConsumption <- subset(powerConsumption, Date == "2007-02-01" | Date == "2007-02-02")
+
+#Convert time character string to date time
+powerConsumption$Time <- strptime(x = paste(powerConsumption$Date, powerConsumption$Time), format = "%Y-%m-%d %H:%M:%S")
+
+#Convert Global Active Power to numeric
+powerConsumption$Global_active_power <- suppressWarnings(as.numeric(as.character(powerConsumption$Global_active_power)))
+
+#Open PNG device
+png('plot1.png')
+
+#Create Chart
+hist(powerConsumption$Global_active_power, main = "Global Active Power", col = "Red", xlab = "Global Active Power (kilowatts)")
+
+#Close the device
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,24 @@
+#Read in file
+powerConsumption <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+
+#Convert character string to Date
+powerConsumption$Date <- as.Date(powerConsumption$Date, "%d/%m/%Y")
+
+#Subset to dates of interest
+powerConsumption <- subset(powerConsumption, Date == "2007-02-01" | Date == "2007-02-02")
+
+#Convert time character string to date time
+powerConsumption$Time <- strptime(x = paste(powerConsumption$Date, powerConsumption$Time), format = "%Y-%m-%d %H:%M:%S")
+
+#Convert Global Active Power to numeric
+powerConsumption$Global_active_power <- suppressWarnings(as.numeric(as.character(powerConsumption$Global_active_power)))
+
+#Open PNG device
+png('plot2.png')
+
+#Create Chart
+with(powerConsumption, plot(Time, Global_active_power, xlab = "", ylab = "Global Active Power (kilowatts)", type = "n"))
+with(powerConsumption, lines(Time, Global_active_power))
+
+#Close the device
+dev.off()
