dc9eb5321bc489c422f1aa122ecb260e66940334
mscafuro13 <mscafuro13@gmail.com>
2018-06-10 17:35:19 -0400

    Completed fourth plot

--- plot4.R
+++ plot4.R
@@ -19,22 +19,27 @@ powerConsumption$Sub_metering_2 <- suppressWarnings(as.numeric(as.character(powe
 powerConsumption$Sub_metering_3 <- suppressWarnings(as.numeric(as.character(powerConsumption$Sub_metering_3)))
 
 #Open PNG device
-#png('plot3.png', width = 480, height = 480)
+png('plot4.png', width = 480, height = 480)
+
+#Create 2 x 2 plot
+par(mfrow = c(2,2), mar = c(5.1,4.1,2.1,2.1))
 
 #Create top left chart
-with(powerConsumption, plot(Time, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)", type = "n"))
+with(powerConsumption, plot(Time, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
 
 #Create top right chart
-#plot(powerConsumption$Time, powerConsumption$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+plot(powerConsumption$Time, powerConsumption$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
 
 #Create lower left chart
-#with(powerConsumption, plot(Time, Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "n"))
-#with(powerConsumption, lines(Time, Sub_metering_1))
-#with(powerConsumption, lines(Time, Sub_metering_2, col = "Red"))
-#with(powerConsumption, lines(Time, Sub_metering_3, col = "Blue"))
+with(powerConsumption, plot(Time, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
+with(powerConsumption, lines(Time, Sub_metering_2, col = "Red"))
+with(powerConsumption, lines(Time, Sub_metering_3, col = "Blue"))
 
 #Create legend
-#legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("Black", "Red", "Blue"), lty = 1)
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("Black", "Red", "Blue"), lty = 1)
+
+#Create Lower Right Chart
+with(powerConsumption, plot(Time, Global_reactive_power, type = "l", xlab = "datetime"))
 
 #Close the device
-#dev.off()
+dev.off()
